56ca5e2b1218208ace41dff6d87c79ee85b864fa
b72u68 <mdinh@hawk.iit.edu>
2020-10-03 00:39:56 -0500

    add read_file, check_message, get word list

--- zoomChatMonitoring.py
+++ zoomChatMonitoring.py
@@ -1,18 +1,54 @@
 class ZoomChatMonitoring:
 
-    def __init__(self, filter_word_file=''):
+    def __init__(self, bad_word_file='', filter_word_file=''):
         self.filter_word_file = filter_word_file
+        self.bad_word_file = bad_word_file
+
         # filter_words: list of words to filter message (read from file)
         self.filter_words = []
+        # bad_words: list of bad words (read from file)
+        self.bad_words = []
+
         # student_messages: {student_name: {message_time: str, message: str}}
         self.student_messages = {}
         # student_questions: {student_name: {message_time: str, questions: []}}
         self.student_questions = {}
 
-    # def read_file(self): get filter_words list
+    def read_file(self, filename):
+        """
+        read words from file
+        """
+        words = []
+        try:
+            file = open(filename, 'rt')
+            words = [word for word in file.readlines()]
 
-    # def check_message(self, message): return True if student message doesn't
-    # contain words from filter_words, otherwise return False
+        except Exception as e:
+            print(f'[-] Error Occurred: {e}')
+
+        return words
+
+    def get_bad_word_list(self):
+        """get bad words from file"""
+        self.bad_words= self.read_file(self.bad_word_file)
+
+    def get_filter_word_list(self):
+        """get filter words from file"""
+        self.filter_words = self.read_file(self.filter_word_file)
+
+    def check_message(self, message):
+        """
+        return -1 if message contains bad words -> remove
+        return 0 if message contains filter words -> no credits
+        return 1 if message can be counted as credits
+        """
+        for word in  self.bad_word_file:
+            if word in message:
+                return -1
+        for word in self.filter_words:
+            if word in message:
+                return 0
+        return 1
 
     # def monitoring_messages(self): check if message is worth for credits ->
     # add to student_messages, if a question -> add to student_questions
