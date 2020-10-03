a9fd298e2f43b0b8832e28e4a1125d492638ce53
b72u68 <mdinh@hawk.iit.edu>
2020-10-03 16:19:51 -0500

    add main file and tests directory

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+from zoomChatMonitoring import ZoomChatMonitoring
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Zoom Chat Monitoring for student extra credits')
+    parser.add_argument('chatfile', help='add chat file directory for monitoring')
+    parser.add_argument('-bw', '--badwords', help='include file contains inappropriate words', default='./badWords.txt')
+    parser.add_argument('-fw', '--filterwords', help='include file contains words to filter messages', default='./filterWords.txt')
+    return parser.parse_args()
+
+def main():
+    # check if log directory exists
+    try:
+        os.mkdir('./zoom_logs')
+    except OSError:
+        # directory already exists
+        pass
+
+    # parse user's arguments
+    args = parse_args()
+    chat_file = args.chatfile
+    bad_word_file = args.badwords
+    filter_word_file = args.filterwords
+
+    # new monitoring
+    chat_monitoring = ZoomChatMonitoring(bad_word_file, filter_word_file, chat_file)
+
+if __name__ == '__main__':
+    main()
