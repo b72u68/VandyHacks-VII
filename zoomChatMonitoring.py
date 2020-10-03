2126be00088c08cda676026c4a675f8f1cdce52d
b72u68 <mdinh@hawk.iit.edu>
2020-10-03 12:03:08 -0500

    add conflicted code

--- zoomChatMonitoring.py
+++ zoomChatMonitoring.py
@@ -1,4 +1,7 @@
-
+import datetime
+import os
+import csv
+import json
 
 class ZoomChatMonitoring:
 
@@ -16,8 +19,9 @@ class ZoomChatMonitoring:
         # bad_words: list of bad words (read from file)
         self.bad_words = []
 
+        # student_messages: {student_email: [{message_time: str, message: str}]}
         self.student_messages = {}
-        # student_questions: {student_name: [{message_time: str, questions: str}]}
+        # student_questions: {student_email: [{message_time: str, questions: str}]}
         self.student_questions = {}
 
     def read_file(self, filename):
@@ -55,3 +59,81 @@ class ZoomChatMonitoring:
             if word in message:
                 return 0
         return 1
+
+    def is_question(self, message):
+        """
+        return True if message is a question
+        return False otherwise
+        """
+        text = message.split(' ')
+
+        # get first word of message
+        firstWord = text[0]
+        # get punctuation
+        lastWord = text[-1][-1]
+
+        if firstWord in self.question_words or lastWord == '?':
+            return True
+
+        return False
+
+    def monitoring_messages(self, json_data):
+        """monitoring and storing message from json_data"""
+
+        # get list of message data (json_data = placeholder)
+        chat_log = json.loads(json_data)
+
+        # iterate through messages
+        for message_obj in chat_log["messages"]:
+            # get student student_email
+            student_email = message_obj["sender"]
+
+            # get student message
+            msg = message_obj["message"]
+            # get message time as a string
+            time = message_obj["date_time"].split('T')[1][0:-1]
+
+            # check if message is worth for student credits
+            if self.check_message(msg) == 1:
+                # if message is a question
+                if self.is_question(msg):
+                    # check if student_email key exists in student_questions
+                    if not self.student_questions[student_email]:
+                        self.student_questions[student_email] = []
+
+                    # add to student_questions
+                    self.student_questions[student_email].append({'message_time': time, 'message': msg})
+
+                #if not question, but worth for credits
+                else:
+                    # check if student_email key exists in student_messages
+                    if not self.student_messages[student_email]:
+                        self.student_messages[student_email] = []
+
+                    # add to student_messages
+                    self.student_messages[student_email].append({'message_times': time, 'message': msg})
+
+    def write_csv(self):
+        """write students who get credits to csv file"""
+        current_date = datetime.datetime.now()
+        filename = f'{current_date.month}-{current_date.day}-{current_date.year}.csv'
+
+        try:
+            # create directory zoom_logs if not exist
+            os.mkdir('./zoom_logs')
+        except OSError:
+            # the directory has already existed
+            pass
+
+        filepath = './zoom_logs' + filename
+
+        with open(filepath, mode='w') as zoom_log_file:
+            fieldnames = ['student_email', 'point']
+            writer = csv.DictWriter(zoom_log_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for student in self.student_messages:
+                writer.writerow({'name': student.student_email, 'point': 1})
+            for student in self.student_questions:
+                if student not in self.student_messages:
+                    writer.writerow({'name': student.student_email, 'point': 1})
