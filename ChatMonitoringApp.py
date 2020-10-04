25af776e1b3eee239e2da044049d04da381b9c8d
Alexis <aedwards4@hawk.iit.edu>
2020-10-04 07:50:33 -0500

    Fixed minor bug related to monitoring with time search

--- ChatMonitoringApp.py
+++ ChatMonitoringApp.py
@@ -13,9 +13,9 @@ chat_file = ''
 badWords_file = './default_word_files/badWords.txt'
 filterWords_file = './default_word_files/filterWords.txt'
 student_names = []
-searchOptionsWind = None
-nameMenuVal = None				#NEW!!!!
-monOptionVal = None				#NEW!!!
+#searchOptionsWind = None
+#nameMenuVal = None				#NEW!!!!
+#monOptionVal = None				#NEW!!!
 
 HourOptionList = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18' ,'19', '20', '21', '22', '23']
 MinuteOptionList = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18' ,'19', '20', '21', '22', '23', '24', '25','26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38','39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51','52', '53', '54', '55', '56', '57', '58', '59']
@@ -60,7 +60,7 @@ uploadLabel.pack()
 
 
 def openSearchOptions():
-	global searchOptionsWind,startHourVal,startMinuteVal,startSecondVal,endHourVal,endMinuteVal,endSecondVal			#NEW
+	global searchOptionsWind,startHourVal,startMinuteVal,startSecondVal,endHourVal,endMinuteVal,endSecondVal, monOptionVal			#NEW
 
 	searchOptionsWind = tk.Toplevel(height=HEIGHT, width=WIDTH, bg=background_color)
 	searchOptionsWind.title("Time Search")
@@ -227,7 +227,7 @@ def start():
 	elif opt3.get() == 1:
 		s = 1
 		useFilt = 0
-		if monOptionVal == "Yes":
+		if monOptionVal.get() == "Yes":
 			useFilt=1																	#NEW (below)
 		result = subprocess.run(['python3', 'main.py', '-m', f'{useFilt}', '-s', f'{s}', '-n', f'{nameMenuVal.get()}', '-st', f'{startHourVal.get()}:{startMinuteVal.get()}:{startSecondVal.get()}', '-e', f'{endHourVal.get()}:{endMinuteVal.get()}:{endSecondVal.get()}', chat_file], shell=False, capture_output=True)
 		messagebox.showinfo(title="Search Results",message=result.stdout)				#NEW
