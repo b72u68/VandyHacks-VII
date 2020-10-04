a0608ea67f05a88d3e309c565d9a5d1040b662bd
b72u68 <mdinh@hawk.iit.edu>
2020-10-04 15:56:47 -0500

    edit README.md

--- ChatMonitoringApp.py
+++ ChatMonitoringApp.py
@@ -16,14 +16,16 @@ chat_file = ''
 badWords_file = './default_word_files/badWords.txt'
 filterWords_file = './default_word_files/filterWords.txt'
 student_names = []
+
 #searchOptionsWind = None
-#nameMenuVal = None				#NEW!!!!
-#monOptionVal = None				#NEW!!!
+#nameMenuVal = None
+#monOptionVal = None
 
 HourOptionList = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18' ,'19', '20', '21', '22', '23']
 MinuteOptionList = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18' ,'19', '20', '21', '22', '23', '24', '25','26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38','39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51','52', '53', '54', '55', '56', '57', '58', '59']
 
 background_color = 'wheat1'
+
 #create base window
 root = tk.Tk()
 root.geometry("300x350")
@@ -31,27 +33,28 @@ root.configure(bg=background_color)
 root.title("Zoom Chat Monitering")
 
 #custom font
-helv12 = tkFont.Font(family="Helvetica",size=12,underline=1)
-helv10 = tkFont.Font(family="Helvetica",size=10)
+helv12 = tkFont.Font(family="Helvetica", size=12, underline=1)
+helv10 = tkFont.Font(family="Helvetica", size=10)
 
-tnr12 = tkFont.Font(family="Times New Roman",size=12)
-tnr11 = tkFont.Font(family="Times New Roman",size=11)
+tnr12 = tkFont.Font(family="Times New Roman", size=12)
+tnr11 = tkFont.Font(family="Times New Roman", size=11)
 
-
-monitorFrame = tk.Frame(root,bg=background_color)									#NEW
-monitorFrame.pack(side=tk.TOP)									#NEW
+monitorFrame = tk.Frame(root,bg=background_color)
+monitorFrame.pack(side=tk.TOP)
 
 #Checkbutton for choosing monitoring option
-optionsLabel = tk.Label(monitorFrame, text="Choose type of monitoring:", font=helv12,bg=background_color)		#NEW CHANGES
+optionsLabel = tk.Label(monitorFrame, text="Choose type of monitoring:", font=helv12, bg=background_color)
 optionsLabel.pack()
 
-opt1=IntVar()
-opt2=IntVar()
-opt3=IntVar()
+opt1 = IntVar()
+opt2 = IntVar()
+opt3 = IntVar()
+
 #creating options i.e. "What kind of monitoring?"
-option1 = tk.Checkbutton(monitorFrame, text = "Participation Grading", font=tnr11,bg=background_color, fg = "black", variable = opt1, onvalue = 1, offvalue = 0)		#NEW
-option2 = tk.Checkbutton(monitorFrame, text = "Inappropriate Language", font=tnr11,bg=background_color, fg = "black", variable = opt2, onvalue = 1, offvalue = 0)		#NEW
-option3 = tk.Checkbutton(monitorFrame, text = "Time Search", font=tnr11,bg=background_color, fg = "black", variable = opt3, onvalue = 1, offvalue = 0, command=lambda:openSearchOptions())	#NEW
+option1 = tk.Checkbutton(monitorFrame, text="Participation Grading", font=tnr11, bg=background_color, fg="black", variable=opt1, onvalue=1, offvalue=0)
+option2 = tk.Checkbutton(monitorFrame, text="Inappropriate Language", font=tnr11, bg=background_color, fg="black", variable=opt2, onvalue=1, offvalue=0)
+option3 = tk.Checkbutton(monitorFrame, text="Time Search", font=tnr11, bg=background_color, fg="black", variable=opt3, onvalue=1, offvalue=0, command=openSearchOptions())
+
 #adding buttons to screen
 option1.pack()
 option2.pack()
@@ -61,194 +64,206 @@ option3.pack()
 uploadLabel = tk.Label(root, text="Upload Files:", font=helv12,bg=background_color)
 uploadLabel.pack()
 
-
 def openSearchOptions():
-	global searchOptionsWind,startHourVal,startMinuteVal,startSecondVal,endHourVal,endMinuteVal,endSecondVal, monOptionVal			#NEW
+    global searchOptionsWind, startHourVal, startMinuteVal, startSecondVal, endHourVal, endMinuteVal, endSecondVal, monOptionVal
 
-	searchOptionsWind = tk.Toplevel(height=HEIGHT, width=WIDTH, bg=background_color)
-	searchOptionsWind.title("Time Search")
-	searchOptionsWind.geometry("250x250")
+    searchOptionsWind = tk.Toplevel(height=HEIGHT, width=WIDTH, bg=background_color)
+    searchOptionsWind.title("Time Search")
+    searchOptionsWind.geometry("250x250")
 
-	startLabel = tk.Label(searchOptionsWind, text="Choose start time:", font=helv12,bg=background_color)
-	startLabel.pack(side=tk.TOP)													#NEW
+    startLabel = tk.Label(searchOptionsWind, text="Choose start time:", font=helv12, bg=background_color)
+    startLabel.pack(side=tk.TOP)
 
-	f1 = tk.Frame(searchOptionsWind)												#NEW
-	f1.pack(side=tk.TOP)															#NEW
+    f1 = tk.Frame(searchOptionsWind)
+    f1.pack(side=tk.TOP)
 
-	startHourVal = StringVar()														#NEW
-	startHour = OptionMenu(f1, startHourVal, *HourOptionList)						#NEW
-	startHour.pack(side=tk.LEFT)													#NEW
+    startHourVal = StringVar()
+    startHour = OptionMenu(f1, startHourVal, *HourOptionList)
+    startHour.pack(side=tk.LEFT)
 
-	startMinuteVal = StringVar()													#NEW
-	startMinute = OptionMenu(f1, startMinuteVal, *MinuteOptionList)					#NEW
-	startMinute.pack(side=tk.LEFT)													#NEW
+    startMinuteVal = StringVar()
+    startMinute = OptionMenu(f1, startMinuteVal, *MinuteOptionList)
+    startMinute.pack(side=tk.LEFT)
 
-	startSecondVal = StringVar()													#NEW
-	startSecond = OptionMenu(f1, startSecondVal, *MinuteOptionList)					#NEW
-	startSecond.pack(side=tk.LEFT)													#NEW
+    startSecondVal = StringVar()
+    startSecond = OptionMenu(f1, startSecondVal, *MinuteOptionList)
+    startSecond.pack(side=tk.LEFT)
 
+    endLabel = tk.Label(searchOptionsWind, text="Choose end time:", font=helv12, bg=background_color)
+    endLabel.pack(side=tk.TOP)
 
-	endLabel = tk.Label(searchOptionsWind, text="Choose end time:", font=helv12,bg=background_color)
-	endLabel.pack(side=tk.TOP)														#NEW
+    f2 = tk.Frame(searchOptionsWind)
+    f2.pack(side=TOP)
 
-	f2 = tk.Frame(searchOptionsWind)												#NEW
-	f2.pack(side=TOP)																#NEW
+    endHourVal = StringVar()
+    endHour = OptionMenu(f2, endHourVal, *HourOptionList)
+    endHour.pack(side=tk.LEFT)
 
+    endMinuteVal = StringVar()
+    endMinute = OptionMenu(f2, endMinuteVal, *MinuteOptionList)
+    endMinute.pack(side=tk.LEFT)
 
-	endHourVal = StringVar()														#NEW
-	endHour = OptionMenu(f2, endHourVal, *HourOptionList)							#NEW
-	endHour.pack(side=tk.LEFT)														#NEW
+    endSecondVal = StringVar()
+    endSecond = OptionMenu(f2, endSecondVal, *MinuteOptionList)
+    endSecond.pack(side=tk.LEFT)
 
-	endMinuteVal = StringVar()														#NEW
-	endMinute = OptionMenu(f2, endMinuteVal, *MinuteOptionList)						#NEW
-	endMinute.pack(side=tk.LEFT)													#NEW
+    optionMonLabel = tk.Label(searchOptionsWind, text="Use filtering?", bg=background_color)
+    optionMonLabel.pack()
 
-	endSecondVal = StringVar()														#NEW
-	endSecond = OptionMenu(f2, endSecondVal, *MinuteOptionList)						#NEW
-	endSecond.pack(side=tk.LEFT)													#NEW
+    monOptionVal = StringVar()
+    monOption = OptionMenu(searchOptionsWind, monOptionVal, "No", "Yes")
+    monOption["menu"].config(bg="khaki1", activebackground="green3")
+    monOption.pack()
 
+    saveButton = tk.Button(searchOptionsWind, text="Save", command=save)
+    saveButton.pack()
 
-	optionMonLabel = tk.Label(searchOptionsWind, text="Use filtering?",bg=background_color)
-	optionMonLabel.pack()
+def save():
+    """
+    save search option result
+    """
+    searchOptionsWind.withdraw()
 
-	monOptionVal = StringVar()
-	monOption = OptionMenu(searchOptionsWind, monOptionVal, "No", "Yes")				#NEW CHANGES
-	monOption["menu"].config(bg="khaki1",activebackground="green3")
-	monOption.pack()
+def searchStudents():
+    """
+    search for student name in class file
+    """
+    file = open(chat_file, 'rt')
+    for data in file.readlines():
+        data_split = data.split(' ', 1)
+        student_name = data_split[1].split(" : ")[0][len("From")+1:]
+        if student_name not in student_names:
+            student_names.append(student_name)
+    student_names.sort()
 
+def get_chatFile():
+    """
+    read and get input chat file
+    """
+    global chat_file
+    chat_file_temp = filedialog.askopenfilename()
+    if chat_file_temp:
+        chat_file = chat_file_temp
+    if opt3.get() == 1:
+        global nameMenuVal
+        searchStudents()
+        nameLabel = tk.Label(root, text="Which student would you like to search for?")
+        nameLabel.pack()
 
-	saveButton = tk.Button(searchOptionsWind, text="Save", command=save)
-	saveButton.pack()
+        nameMenuVal = StringVar()
+        nameMenu = OptionMenu(root, nameMenuVal, *student_names)
+        nameMenu.pack()
 
-#
-def save():																				#NEW
-	searchOptionsWind.withdraw()														#NEW
+def get_badWordsFile():
+    """
+    get bad word file from input
+    """
+    global badWords_file
+    badWords_file_temp = filedialog.askopenfilename()
+    if badWords_file_temp:
+        badWords_file = badWords_file_temp
+    w1.withdraw()
 
-
-def searchStudents():																	#NEW:
-	file = open(chat_file, 'rt')														#NEW
-	for data in file.readlines():														#NEW
-		data_split = data.split(' ', 1)													#NEW
-		student_name = data_split[1].split(" : ")[0][len("From")+1:]					#NEW
-		if student_name not in student_names:											#NEW
-			student_names.append(student_name)											#NEW
-	student_names.sort()																#NEW^^^
-
-
-#function for opening files in read mode
-def open_chatFile():
-	global chat_file
-	chat_file_temp = filedialog.askopenfilename()
-	if chat_file_temp:
-		chat_file = chat_file_temp
-	if opt3.get() == 1:
-		global nameMenuVal																	#NEW
-		searchStudents()																	#NEW
-		nameLabel = tk.Label(root, text="Which student would you like to search for?")		#NEW
-		nameLabel.pack()																	#NEW
-
-		nameMenuVal = StringVar()															#NEW
-		nameMenu = OptionMenu(root, nameMenuVal, *student_names)							#NEW
-		nameMenu.pack()																		#NEW^^^
-
-def open_badWordsFile():
-	global badWords_file
-	badWords_file_temp = filedialog.askopenfilename()
-	if badWords_file_temp:
-		badWords_file = badWords_file_temp
-	w1.withdraw()
-
-def open_filterChatFile():
-	global filterWords_file
-	filterWords_file_temp = filedialog.askopenfilename()
-	if filterWords_file_temp:
-		filterWords_file = filterWords_file_temp
-	w2.withdraw()
+def get_filterChatFile():
+    """
+    get filter word file from input
+    """
+    global filterWords_file
+    filterWords_file_temp = filedialog.askopenfilename()
+    if filterWords_file_temp:
+        filterWords_file = filterWords_file_temp
+    w2.withdraw()
 
 
 #Open chat_file
-chatFileLabel = tk.Label(root, text = "Upload chat log:", font=tnr11,bg=background_color)		#NEW CHANGES
+chatFileLabel = tk.Label(root, text = "Upload chat log:", font=tnr11, bg=background_color)
 chatFileLabel.pack()
-openChatFileButton = tk.Button(root, text="Browse", font=helv10, bg='khaki1', command=lambda:open_chatFile())			#NEW CHANGES
+openChatFileButton = tk.Button(root, text="Browse", font=helv10, bg='khaki1', command=get_chatFile())
 openChatFileButton.pack()
 
 
 #Open badWords_file
-badWordFileLabel = tk.Label(root, text = "Upload list of inappropriate words:", font=tnr11,bg=background_color)					#NEW
+badWordFileLabel = tk.Label(root, text = "Upload list of inappropriate words:", font=tnr11,bg=background_color)
 badWordFileLabel.pack()
-#openBadWordsFileButton = tk.Button(root, text="Browse", font=helv10, bg='khaki1', command=lambda:open_badWordsFile())
+#openBadWordsFileButton = tk.Button(root, text="Browse", font=helv10, bg='khaki1', command=open_badWordsFile())
 #openBadWordsFileButton.pack()
 
 #Use default or custom file?
-badOpt1 = badOpt2 = None																				#NEW
-bad1 = tk.Checkbutton(root, text = "Default", font=tnr11,bg=background_color, fg = "black", variable = badOpt1, onvalue = 1, offvalue = 0)		#NEW
-bad1.pack()																				#NEW
-bad2 = tk.Checkbutton(root, text = "Custom", font=tnr11,bg=background_color, fg = "black", variable = badOpt2, onvalue = 1, offvalue = 0,command=lambda:badSearch())		#NEW
+badOpt1 = badOpt2 = None
+bad1 = tk.Checkbutton(root, text="Default", font=tnr11, bg=background_color, fg="black", variable=badOpt1, onvalue=1, offvalue=0)
+bad1.pack()
+bad2 = tk.Checkbutton(root, text="Custom", font=tnr11, bg=background_color, fg="black", variable=badOpt2, onvalue=1, offvalue=0, command=badSearch())
 bad2.pack()
 
 #Prompt for custom file
 def badSearch():
-	global w1
-	w1 = tk.Toplevel(height=(HEIGHT/2), width=(WIDTH/2), bg=background_color)
-	w1.title("Browse File")
-	#w1.geometry("250x250")
+    """
+    browse bad word file
+    """
+    global w1
+    w1 = tk.Toplevel(height=(HEIGHT/2), width=(WIDTH/2), bg=background_color)
+    w1.title("Browse File")
+    #w1.geometry("250x250")
 
-	openBadWordsFileButton = tk.Button(w1, text="Browse", command=lambda:open_badWordsFile())
-	openBadWordsFileButton.pack()
+    openBadWordsFileButton = tk.Button(w1, text="Browse", command=get_badWordsFile())
+    openBadWordsFileButton.pack()
 
 
 #Open filterWords_file
-filterWordsFileLabel = tk.Label(root, text = "Upload list of filter words:", font=tnr11,bg=background_color)
+filterWordsFileLabel = tk.Label(root, text="Upload list of filter words:", font=tnr11, bg=background_color)
 filterWordsFileLabel.pack()
-#openFilterWordsButton = tk.Button(root, text="Browse", font=helv10, bg='khaki1', command=lambda:open_filterChatFile())
+#openFilterWordsButton = tk.Button(root, text="Browse", font=helv10, bg='khaki1', command=open_filterChatFile())
 #openFilterWordsButton.pack()
 
 #Use default or custom file?
-filterOpt1 = filterOpt2 = None																				#NEW
-filter1 = tk.Checkbutton(root, text = "Default", font=tnr11,bg=background_color, fg = "black", variable = filterOpt1, onvalue = 1, offvalue = 0)		#NEW
-filter1.pack()																				#NEW
-filter2 = tk.Checkbutton(root, text = "Custom", font=tnr11,bg=background_color, fg = "black", variable = filterOpt2, onvalue = 1, offvalue = 0,command=lambda:filterSearch())		#NEW
+filterOpt1 = filterOpt2 = None
+filter1 = tk.Checkbutton(root, text="Default", font=tnr11, bg=background_color, fg="black", variable=filterOpt1, onvalue=1, offvalue=0)
+filter1.pack()
+filter2 = tk.Checkbutton(root, text="Custom", font=tnr11, bg=background_color, fg="black", variable=filterOpt2, onvalue=1, offvalue=0, command=filterSearch())
 filter2.pack()
 
 def filterSearch():
-	global w2
-	w2 = tk.Toplevel(height=(HEIGHT/2), width=(WIDTH/2), bg=background_color)
-	w2.title("Browse File")
-	#w2.geometry("250x250")
+    """
+    browse filter word file
+    """
+    global w2
+    w2 = tk.Toplevel(height=(HEIGHT/2), width=(WIDTH/2), bg=background_color)
+    w2.title("Browse File")
+    #w2.geometry("250x250")
 
-	openFilterWordsButton = tk.Button(w2, text="Browse", font=helv10, bg='khaki1', command=lambda:open_filterChatFile())
-	openFilterWordsButton.pack()
-
+    openFilterWordsButton = tk.Button(w2, text="Browse", font=helv10, bg='khaki1', command=get_filterChatFile())
+    openFilterWordsButton.pack()
 
 def start():
     python_command = 'python3'
-    python_path = sys.executable
+    # python_path = sys.executable
     machine_os = platform.system()
     if machine_os == 'Windows':
-        python_command = python_path.split('\\')[-1].split('.')[0]
+        # python_command = python_path.split('\\')[-1].split('.')[0]
+        python_command = 'python'
 
     if opt1.get() == 1:
-        result = subprocess.run([python_command, 'main.py', '-fw', filterWords_file, chat_file], shell=False, capture_output=True)		#NEW CHANGES
-        messagebox.showinfo(title="Search Results",message=result.stdout)
+        result = subprocess.run([python_command, 'main.py', '-fw', filterWords_file, chat_file], shell=False, capture_output=True)
+        messagebox.showinfo(title="Search Results", message=result.stdout)
 
     elif opt2.get() == 1:
-        result = subprocess.run(['python3', 'main.py', '-bw', badWords_file, chat_file], shell=False, capture_output=True)				#NEW CHANGES
-        messagebox.showinfo(title="Search Results",message=result.stdout)															#NEW
-	# elif opt3.get() == 1:
-		# s = 1
-		# useFilt = 0
-		# if monOptionVal.get() == "Yes":
-			# useFilt=1																	#NEW (below)
-		# result = subprocess.run(['python3', 'main.py', '-m', f'{useFilt}', '-s', f'{s}', '-n', f'{nameMenuVal.get()}', '-st', f'{startHourVal.get()}:{startMinuteVal.get()}:{startSecondVal.get()}', '-e', f'{endHourVal.get()}:{endMinuteVal.get()}:{endSecondVal.get()}', chat_file], shell=False, capture_output=True)
-		# messagebox.showinfo(title="Search Results",message=result.stdout)				#NEW
+        result = subprocess.run(['python3', 'main.py', '-bw', badWords_file, chat_file], shell=False, capture_output=True)
+        messagebox.showinfo(title="Search Results", message=result.stdout)
+
+    # elif opt3.get() == 1:
+        # s = 1
+        # useFilt = 0
+        # if monOptionVal.get() == "Yes":
+            # useFilt=1
+        # result = subprocess.run(['python3', 'main.py', '-m', f'{useFilt}', '-s', f'{s}', '-n', f'{nameMenuVal.get()}', '-st', f'{startHourVal.get()}:{startMinuteVal.get()}:{startSecondVal.get()}', '-e', f'{endHourVal.get()}:{endMinuteVal.get()}:{endSecondVal.get()}', chat_file], shell=False, capture_output=True)
+        # messagebox.showinfo(title="Search Results",message=result.stdout)
 
     else:
-        messagebox.showerror(title="Error",message="You must select one monitoring option.")			#NEW
-
-
+        messagebox.showerror(title="Error", message="You must select one monitoring option.")
 
 #creating a button
-startButton = tk.Button(root, text="Start", fg = "black", bg = "green3", command=start, font=helv12)
+startButton = tk.Button(root, text="Start", fg="black", bg="green3", command=start, font=helv12)
+
 #put button on screen
 startButton.pack()
 
--- README.md
+++ README.md
@@ -2,33 +2,75 @@
 
 ## Project for VandyHacks VII
 
-This app is intended for teachers who use Zoom for their online teaching purposes. 
+This app is intended for teachers who use Zoom for their online teaching purposes.
 There are a few functions it can perform: checking for chat participation, flagging
-inappropriate language, or searching for chats from a specific student. During this 
-time of increased remote classwork, this can be a useful tool for teachers to 
-obtain quick stats on their students' involvement in the Zoom classroom, through 
+inappropriate language, or searching for chats from a specific student. During this
+time of increased remote classwork, this can be a useful tool for teachers to
+obtain quick stats on their students' involvement in the Zoom classroom, through
 smooth interaction on a user-friendly GUI.
 
+## What problem are we solving
+
+With COVID-19, Zoom usage dramatically increased, especially for universities.
+This presented an obstacle for classes that rely on participation. Some professors
+have chosen to document participation by manually scanning the Zoom chat for names.
+This is extremely time consuming and leaves lots of room for error.
+
+## What it does
+
+Our program will scan Zoom files using given parameters. One function will scan
+all messages, and award a point to each student that submits “meaningful”
+participation, meaning that it was relevant to the lecture and not simply a
+greeting or interjection such as “oh.” The program will output a .csv file of
+grades for each student’s participation that day. Another feature will scan the
+chat log for any inappropriate language, flagging each individual and their
+associated message. The program will output a .csv file of flagged messages and
+students for further review. The final feature will search the chat log within
+a specified time for a specific student’s contributions, and return the search
+results to the screen.
 
 ## Usage
 
-	- After concluding a Zoom meeting, download the chat and note the file 
-	  location of where it saves. 
-	- Open the dist folder.
-	- Run the executable to start up the interface. 
-		**WARNING: only tested on Windows OS**
-	- Choose the option desired. (Please check only one box!)
-	- Under "Upload chat log," navigate to the file that contains the Zoom chat.
-	- If you would like to provide a custom list of inappropriate words or filter
-	  words (those which do not denote meaningful input, like "lol" or "oh"), 
-	  attach them under their respective tabs.
-	- Click the Start button.
-	- Once finished, the output will be written to a .csv file. 
+    - After concluding a Zoom meeting, download the chat and note the file
+      location of where it saves.
+    - Open the dist folder.
+    - Run the executable to start up the interface.
+    	**WARNING: only tested on Windows OS**
+    - Choose the option desired. (Please check only one box!)
+    - Under "Upload chat log," navigate to the file that contains the Zoom chat.
+    - If you would like to provide a custom list of inappropriate words or filter
+      words (those which do not denote meaningful input, like "lol" or "oh"),
+      attach them under their respective tabs.
+    - Click the Start button.
+    - Once finished, the output will be written to a .csv file.
 
+## Challenges we ran into
+
+The Zoom API does not allow us to get in-meeting chat asynchronously so we have
+to switch to downloading chat in .txt file format. Since each member uses different
+operation system, we had encountered some problem analyzing directory and run
+the app using command line on the terminal and cmd for Windows. Moreover, we
+wanted to experiment with building GUI app using Python so there were a lot
+research had been done and bugs encountered.
+
+## What we have learned
+
+We have learned how to use Github for group project, build Python GUI app.
+
+## Future plan
+
+This is applicable to the future because COVID-19 has likely changed the world
+forever because it has shown us that online and remote work is possible, for
+both students, faculty, and professionals. Zoom will likely be used now more
+than ever, and it will be necessary to track chat messages for things like
+classes or meetings. Furthermore, Zoom API developers have expressed that chat
+APIs are coming soon, which would allow chat messages to be tracked in real time.
+This project could also be generalized for other Chat applications such as
+Discord, Slack, or WhatsApp.
 
 ## Team Members
 
-	- Alexis Edwards
-	- My Dinh
-	- Gladys Toledo-Rodriguez
-	- Jackie McAninch
+    - Alexis Edwards
+    - My Dinh
+    - Gladys Toledo-Rodriguez
+    - Jackie McAninch
